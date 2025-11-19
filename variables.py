acafc1c912dfd4f960ac12d4a4c82e0df555701e
marwan-azami <merouane.elazami@gmail.com>
2025-11-19 13:31:00 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,88 @@
+# Variable
+# Indentifier = value
+# - ✅ Must contain only 0-9, a-z, A-Z and underscore (_)
+# - ✅ Can not start with a digit
+# - ✅ Identifiers are case sensitive
+# -  Using reserved keyword could be source of errors
+# Correct
+age = 25
+AGE = 29
+age_1 = 25
+age2=30
+a9ge = 25
+nom = 'Person'
+num_cin = "ABC343"
+
+#print(age)
+#print(num_cin)
+
+# Data types
+
+PI = 3.14 # floating value
+age = 25
+name = "person"
+
+# Data type int
+variable_1 = 10
+variable_3 = -10
+# Data type float
+variable_float = 3.5335
+variable_ep = 0.1e-10
+# data type str
+variable_name = "Name"
+variable_address = 'Rue n7, FST Hoceima'
+
+print(variable_address)
+
+variable_address = 3.14
+
+print(variable_address)
+
+# Data type boolean
+is_night = False
+is_raining = False
+is_november = True
+print(type(is_november))
+# Data type list
+
+class_1_cycle_ing = [ "Mohamed", "Nadia", "Walid" ]
+
+random_samples = [True, 3.20, "Hello", 2025]
+
+samples_1 = [ 3.12, 0.2, 0.5, 1, 3.2, 1, 0.5]
+
+# ------------------------- remove # if you want to print
+# print(random_samples[2])
+# print(class_1_cycle_ing[2])
+# print(samples_1[0])
+# # Index
+# Length len() input list, -> size (int)
+# balbala = "kslkfklsdfsdhfkljshdkfljhsdq"
+# size_of_samples = len(samples_1)
+# print("La taille ", size_of_samples)
+#
+# #
+# print(len(balbala))
+# print(balbala[29])
+# size_ = len(balbala)-1
+# print(balbala[0])
+
+
+
+
+
+message = input("Donnez moi une chaine de caractere!")
+
+print(message)
+
+_size = len(message)
+
+print(_size)
+
+print(message[_size - 1])
+
+
+
+
+
+
